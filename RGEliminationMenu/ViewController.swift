8ba842e3e28a480916fa4a45927e0e36054115b3
Roman Gille <developer@romangille.com>
2016-04-14 10:32:26 +0200

    add menu, created in code

--- RGEliminationMenu/ViewController.swift
+++ RGEliminationMenu/ViewController.swift
@@ -11,30 +11,57 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var eliminationMenu: RGEliminationMenu!
+    @IBOutlet weak var infoLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up the menu created in interface builder.
+        
         eliminationMenu.items = [
             MenuItem(title: "First", value: "SomeValue"),
             MenuItem(title: "Second", value: "SomeOtherValue"),
             MenuItem(title: "Third", value: "This could also be an Image"),
             MenuItem(title: "Fourth", value: "...or a view")
         ]
-        eliminationMenu.align = .Right
         
         eliminationMenu.selectionHandler = {item in
             let menuItem = item as! MenuItem
             
-            let alert = UIAlertController(
-                title: "Value selected",
-                message: "Selected item \"\(menuItem.title)\" with value \"\(menuItem.value)\"",
-                preferredStyle: .Alert
-            )
-            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-            
-            self.presentViewController(alert, animated: true, completion: nil)
+            self.infoLabel.text = "Selected item:\n\"\(menuItem.title)\"\n\nThe value is:\n\"\(menuItem.value)\""
         }
+        
+        // Create nemu in code.
+        
+        let rightMenu = RGEliminationMenu()
+        
+        rightMenu.items = [
+            MenuItem(title: "First on the right", value: "SomeValue"),
+            MenuItem(title: "Second", value: "SomeOtherValue"),
+            MenuItem(title: "Third", value: "This could also be an Image"),
+            MenuItem(title: "Fourth", value: "...or some other class")
+        ]
+        
+        rightMenu.selectionHandler = {item in
+            let menuItem = item as! MenuItem
+            
+            self.infoLabel.text = "Selected item:\n\"\(menuItem.title)\",\n\nThe value is:\n\"\(menuItem.value)\""
+        }
+        
+        rightMenu.align = .Right
+        
+        // Add menu to layout.
+        
+        rightMenu.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rightMenu)
+        
+        view.addConstraints([
+            NSLayoutConstraint(item: view, attribute: .RightMargin, relatedBy: .Equal, toItem: rightMenu, attribute: .Right, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .BottomMargin, relatedBy: .Equal, toItem: rightMenu, attribute: .Bottom, multiplier: 1, constant: 20)
+            ])
+        
+        view.setNeedsUpdateConstraints()
+        view.updateConstraintsIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
