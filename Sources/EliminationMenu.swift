ac0c943df222b14ee8841512a8a0bdaf5fbbdd3a
Roman Gille <developer@romangille.com>
2016-04-19 19:02:16 +0200

    remove semicolons. good bye!

--- Sources/EliminationMenu.swift
+++ Sources/EliminationMenu.swift
@@ -32,7 +32,7 @@ public class EliminationMenu: UIView {
     /// The font used in the menu entry buttons. Defaults to systemFontSize().
     public var font = UIFont.systemFontOfSize(UIFont.systemFontSize())
     /// The color of the text in menu entries. Defaults to darkTextColor().
-    public var color = UIColor.darkTextColor();
+    public var color = UIColor.darkTextColor()
     /// The amount of points, the menu entries are offset from the screen before fading tehm in. Defaults to 50.
     public var buttonAnimationOffset = CGFloat(50)
     /// The duration in seconds, that the show animation will take. Defaults to 0.4.
@@ -49,14 +49,14 @@ public class EliminationMenu: UIView {
     /// A closure to react on menu opening/closing. This will be called before the animation runs.
     public var willAnimateHandler: (opening:Bool, animated:Bool) -> Void = {arg in}
     /// A closure to react on menu opening/closing. This will be called after the animation has finished.
-    public var didAnimateHandler: (opening:Bool, animated:Bool) -> Void = {arg in};
+    public var didAnimateHandler: (opening:Bool, animated:Bool) -> Void = {arg in}
     
     var buttons = [UIButton]()
     var extended = false
     
-    private var _items = [Item]();
-    private var _selectedIndex = 0;
-    private let tagOffset = 10;
+    private var _items = [Item]()
+    private var _selectedIndex = 0
+    private let tagOffset = 10
     
     /// The entries of the menu. Setting this will reinitialize the menu.
     public var items: [Item] {
@@ -70,150 +70,150 @@ public class EliminationMenu: UIView {
             
             // Create first button.
             if buttons.count == 0 {
-                let menuItem = newValue[0];
-                let button = self.createButton(menuItem, tag: tagOffset);
+                let menuItem = newValue[0]
+                let button = self.createButton(menuItem, tag: tagOffset)
                 
-                button.frame = CGRectMake(0, 0, max(button.intrinsicContentSize().width, self.bounds.size.width), self.buttonHeight);
+                button.frame = CGRectMake(0, 0, max(button.intrinsicContentSize().width, self.bounds.size.width), self.buttonHeight)
                 
                 buttons.append(button)
-                self.addSubview(button);
+                self.addSubview(button)
                 
-                self.invalidateIntrinsicContentSize();
+                self.invalidateIntrinsicContentSize()
             }
             
-            _items = newValue;
-            _selectedIndex = 0;
+            _items = newValue
+            _selectedIndex = 0
         }
     }
     
     /// The button for the selected item. This is the one that you see when the menu is closed.
     var mainButton: UIButton {
         get {
-            return buttonFor(_selectedIndex)!;
+            return buttonFor(_selectedIndex)!
         }
     }
     
     func buttonPressed(sender: UIView) {
-        let index = sender.tag - tagOffset;
+        let index = sender.tag - tagOffset
         
         if (index == _selectedIndex) {
-            show((buttons.count < 2), animated: true);
+            show((buttons.count < 2), animated: true)
         }
         else {
-            select(sender.tag - tagOffset, animated: true);
+            select(sender.tag - tagOffset, animated: true)
         }
     }
     
     func buttonFor(index: Int) -> UIButton? {
-        return self.viewWithTag(tagOffset + index) as? UIButton;
+        return self.viewWithTag(tagOffset + index) as? UIButton
     }
     
     func createButton(item:Item, tag:Int) -> UIButton {
-        let button = UIButton();
+        let button = UIButton()
         
-        button.setTitle(item.title, forState: .Normal);
-        button.setImage(item.icon, forState: .Normal);
+        button.setTitle(item.title, forState: .Normal)
+        button.setImage(item.icon, forState: .Normal)
         button.contentHorizontalAlignment = (horizontalAlign == .Left) ? .Left : .Right
-        button.tag = tag;
-        button.titleLabel?.font = self.font;
-        button.setTitleColor(color, forState: .Normal);
-        button.addTarget(self, action: #selector(buttonPressed), forControlEvents: .TouchUpInside);
+        button.tag = tag
+        button.titleLabel?.font = self.font
+        button.setTitleColor(color, forState: .Normal)
+        button.addTarget(self, action: #selector(buttonPressed), forControlEvents: .TouchUpInside)
         
-        setIconInsets(item.iconInsets, button: button);
+        setIconInsets(item.iconInsets, button: button)
         
-        return button;
+        return button
     }
     
     func setIconInsets(iconInsets:UIEdgeInsets, button:UIButton) {
-        button.imageEdgeInsets = UIEdgeInsets(top: iconInsets.top, left: iconInsets.left, bottom: iconInsets.bottom, right: 0);
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: iconInsets.right, bottom: 0, right: -iconInsets.right);
-        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: iconInsets.right);
+        button.imageEdgeInsets = UIEdgeInsets(top: iconInsets.top, left: iconInsets.left, bottom: iconInsets.bottom, right: 0)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: iconInsets.right, bottom: 0, right: -iconInsets.right)
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: iconInsets.right)
     }
     
     func show(show: Bool, animated: Bool) {
         if show {
-            var buttonIndex = 0;
-            var buttonY:CGFloat = 0;
+            var buttonIndex = 0
+            var buttonY:CGFloat = 0
             
-            self.willAnimateHandler(opening: true, animated:animated);
+            self.willAnimateHandler(opening: true, animated:animated)
             
             // Add buttons for unselected items.
             for titleIndex in 0..<_items.count {
                 if titleIndex != _selectedIndex {
-                    let menuItem = _items[titleIndex];
-                    let button = self.createButton(menuItem, tag: titleIndex + tagOffset);
+                    let menuItem = _items[titleIndex]
+                    let button = self.createButton(menuItem, tag: titleIndex + tagOffset)
                     
-                    button.frame = CGRectMake(0, buttonY * CGFloat(buttonIndex ), button.intrinsicContentSize().width, self.buttonHeight);
+                    button.frame = CGRectMake(0, buttonY * CGFloat(buttonIndex ), button.intrinsicContentSize().width, self.buttonHeight)
                     
                     buttons.append(button)
-                    self.addSubview(button);
-                    buttonY = self.buttonHeight + margin;
+                    self.addSubview(button)
+                    buttonY = self.buttonHeight + margin
                     buttonIndex += 1
                 }
             }
             
             // Set all buttons to the same width and transform them out of viewport.
-            let maxWidth = max(self.intrinsicContentSize().width, self.bounds.size.width);
+            let maxWidth = max(self.intrinsicContentSize().width, self.bounds.size.width)
             for i in 0..<buttons.count {
                 let button = buttons[i]
-                button.frame = CGRect(origin: button.frame.origin, size: CGSize(width: maxWidth, height: button.frame.size.height));
+                button.frame = CGRect(origin: button.frame.origin, size: CGSize(width: maxWidth, height: button.frame.size.height))
                 
                 if button.tag != _selectedIndex + tagOffset {
-                    let xOffset = (CGFloat(buttons.count) * buttonAnimationOffset) - (buttonAnimationOffset * CGFloat(i));
+                    let xOffset = (CGFloat(buttons.count) * buttonAnimationOffset) - (buttonAnimationOffset * CGFloat(i))
                     
                     if (self.horizontalAlign == .Right) {
-                        button.transform = CGAffineTransformMakeTranslation(maxWidth + xOffset, 0);
+                        button.transform = CGAffineTransformMakeTranslation(maxWidth + xOffset, 0)
                     }
                     else {
-                        button.transform = CGAffineTransformMakeTranslation(-xOffset, 0);
+                        button.transform = CGAffineTransformMakeTranslation(-xOffset, 0)
                     }
                 }
             }
             
-            mainButton.frame = CGRectMake(0, buttonY * CGFloat(buttons.count - 1), maxWidth, self.buttonHeight);
+            mainButton.frame = CGRectMake(0, buttonY * CGFloat(buttons.count - 1), maxWidth, self.buttonHeight)
             
             // Update content size.
-            self.invalidateIntrinsicContentSize();
+            self.invalidateIntrinsicContentSize()
             
             // Slide in buttons.
             UIView.animateWithDuration(showAnimationDuration, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.3, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
                 for button in self.buttons {
-                    button.transform = CGAffineTransformIdentity;
+                    button.transform = CGAffineTransformIdentity
                 }
             }, completion:{(success) -> Void in
-                self.didAnimateHandler(opening: true, animated:animated);
-            });
+                self.didAnimateHandler(opening: true, animated:animated)
+            })
         }
         else {
-            self.select(_selectedIndex, animated: animated);
+            self.select(_selectedIndex, animated: animated)
         }
     }
     
     func select(index: Int?, animated: Bool) {
-        let selectedIndex: Int = index ?? 0;
-        let menuItem = _items[selectedIndex];
+        let selectedIndex: Int = index ?? 0
+        let menuItem = _items[selectedIndex]
         
         if buttons.count > 1 {
-            let selectionDidChange = _selectedIndex != selectedIndex;
+            let selectionDidChange = _selectedIndex != selectedIndex
             
             if selectionDidChange {
-                self.selectionHandler(selectedIdtem: menuItem);
+                self.selectionHandler(selectedIdtem: menuItem)
             }
-            self.willAnimateHandler(opening: false, animated:animated);
+            self.willAnimateHandler(opening: false, animated:animated)
             
-            let selectedButton = buttonFor(selectedIndex);
-            let selectionTargetFrame = CGRect(origin: self.mainButton.frame.origin, size: selectedButton!.frame.size);
-            let nonSelectedTransfromation = CGAffineTransformMakeTranslation(0, self.frame.size.height - selectedButton!.frame.origin.y - selectedButton!.frame.size.height);
+            let selectedButton = buttonFor(selectedIndex)
+            let selectionTargetFrame = CGRect(origin: self.mainButton.frame.origin, size: selectedButton!.frame.size)
+            let nonSelectedTransfromation = CGAffineTransformMakeTranslation(0, self.frame.size.height - selectedButton!.frame.origin.y - selectedButton!.frame.size.height)
             
             UIView.animateWithDuration((animated ? selectAnimationDuration : 0), animations: { () -> Void in
                 for button in self.buttons {
                     if button != selectedButton {
-                        button.transform = nonSelectedTransfromation;
-                        button.alpha = 0;
+                        button.transform = nonSelectedTransfromation
+                        button.alpha = 0
                     }
                 }
                 
-                selectedButton?.frame = selectionTargetFrame;
+                selectedButton?.frame = selectionTargetFrame
 
             }, completion: { (success) -> Void in
                 // Remove hidden buttons.
@@ -230,57 +230,57 @@ public class EliminationMenu: UIView {
                 selectedButton?.frame = CGRect(
                     origin: CGPoint(x: 0, y: 0),
                     size: CGSize(width: selectedButton!.intrinsicContentSize().width, height: self.buttonHeight)
-                );
+                )
                 
-                self._selectedIndex = selectedIndex;
-                self.invalidateIntrinsicContentSize();
+                self._selectedIndex = selectedIndex
+                self.invalidateIntrinsicContentSize()
                 
-                self.didAnimateHandler(opening: true, animated:animated);
+                self.didAnimateHandler(opening: true, animated:animated)
             })
         }
         else {
-            selectionHandler(selectedIdtem: menuItem);
-            self.willAnimateHandler(opening: false, animated:animated);
+            selectionHandler(selectedIdtem: menuItem)
+            self.willAnimateHandler(opening: false, animated:animated)
             
-            mainButton.setTitle(menuItem.title, forState: .Normal);
-            mainButton.setImage(menuItem.icon, forState: .Normal);
+            mainButton.setTitle(menuItem.title, forState: .Normal)
+            mainButton.setImage(menuItem.icon, forState: .Normal)
             
-            setIconInsets(menuItem.iconInsets, button: mainButton);
+            setIconInsets(menuItem.iconInsets, button: mainButton)
             
             mainButton.frame = CGRect(
                 origin: mainButton.frame.origin,
                 size: CGSize(width: mainButton.intrinsicContentSize().width, height: self.buttonHeight)
-            );
-            mainButton.tag = selectedIndex + tagOffset;
-            _selectedIndex = selectedIndex;
+            )
+            mainButton.tag = selectedIndex + tagOffset
+            _selectedIndex = selectedIndex
             
-            self.didAnimateHandler(opening: true, animated:animated);
+            self.didAnimateHandler(opening: true, animated:animated)
         }
     }
     
     func selectEntryWithValue(value: AnyObject!, animated: Bool) {
         for i in 0..<items.count {
             if items[i].value.isEqual(value) {
-                select(i, animated: animated);
+                select(i, animated: animated)
             }
         }
     }
     
     override public func intrinsicContentSize() -> CGSize {
         if buttons.count > 0 {
-            var width: CGFloat = 0;
-            var height: CGFloat = -margin;
+            var width: CGFloat = 0
+            var height: CGFloat = -margin
             
             for button in buttons {
-                let contentSize = button.intrinsicContentSize();
-                height += self.buttonHeight + margin;
+                let contentSize = button.intrinsicContentSize()
+                height += self.buttonHeight + margin
                 if contentSize.width > width {
-                    width = contentSize.width;
+                    width = contentSize.width
                 }
             }
-            return CGSizeMake(width, height);
+            return CGSizeMake(width, height)
         }
-        return CGSizeMake(0, 0);
+        return CGSizeMake(0, 0)
     }
     
     // MARK: - Class: Item
@@ -293,7 +293,7 @@ public class EliminationMenu: UIView {
         /// The object that you will receive in the selectionHandler.
         public var value: AnyObject!
         /// These insets will also set titleInsets and contentInsets of the items entry button.
-        public var iconInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0);
+        public var iconInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         
         init(value:AnyObject, title: String = "", icon: UIImage? = nil) {
             self.title = title
